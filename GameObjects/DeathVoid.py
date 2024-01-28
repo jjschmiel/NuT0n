ab6752aa32a1441b5615e2b0b1c1040c0945a594
Jonathan Schmiel <jjschmiel@gmail.com>
2024-01-28 17:25:33 -0500

    animated death void

--- GameObjects/DeathVoid.py
+++ GameObjects/DeathVoid.py
@@ -1,14 +1,31 @@
 import pygame
 
+death_void_images = [
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_000.png'), 
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_001.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_002.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_003.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_004.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_005.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_006.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_007.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_008.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_009.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_010.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_011.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_012.png'),
+        pygame.image.load('Assets/DeathVoid/GLOOMBLOOM_013.png')
+    ]
+
 class DeathVoid(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, image_path):
+    def __init__(self, x, y, width, height, image_path, animationCounter):
         super().__init__()
         self.image = pygame.Surface((width, height))
-        #self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        
+        self.animationCounter = animationCounter
+        self.animationLength = 14
         
         loaded_image = pygame.image.load(image_path)
         image_width, image_height = loaded_image.get_size()
@@ -17,5 +34,11 @@ class DeathVoid(pygame.sprite.Sprite):
             for j in range(0, height, image_height):
                 self.image.blit(loaded_image, (i, j))
 
+    def update_animation(self):
+        self.animationCounter += 1
+        if self.animationCounter >= self.animationLength:
+            self.animationCounter = 0
+
     def draw(self, window):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+        img = death_void_images[self.animationCounter]
+        window.blit(img, (self.rect.x, self.rect.y))
--- GameObjects/Environment.py
+++ GameObjects/Environment.py
@@ -20,7 +20,13 @@ class Environment():
             Panel(100, -PANEL_HEIGHT + PANEL_SCREEN_HEIGHT_DIFF,  WALL_WIDTH, WALL_HEIGHT, 'Assets/Panels/leftPanel.png'),
             Panel(WIDTH - 575, -PANEL_HEIGHT + PANEL_SCREEN_HEIGHT_DIFF,  WALL_WIDTH, WALL_HEIGHT, 'Assets/Panels/rightPanel.png')
         ]
-        self.deathVoid = DeathVoid((WIDTH // 2) - 200, HEIGHT - 50, 450, 50, 'Assets/DeathVoid/deathVoid.gif')
+        self.deathVoid0 = DeathVoid((WIDTH // 2) - 200, HEIGHT - 50, 450, 50, 'Assets/DeathVoid/deathVoid.gif',0)
+        self.deathVoid1 = DeathVoid((WIDTH // 2) - 132, HEIGHT - 50, 450, 50, 'Assets/DeathVoid/deathVoid.gif',2)
+        self.deathVoid2 = DeathVoid((WIDTH // 2) - 64, HEIGHT - 50, 450, 50, 'Assets/DeathVoid/deathVoid.gif',4)
+        self.deathVoid3 = DeathVoid((WIDTH // 2) + 4 , HEIGHT - 50, 450, 50, 'Assets/DeathVoid/deathVoid.gif',6)
+        self.deathVoid4 = DeathVoid((WIDTH // 2) + 72, HEIGHT - 50, 450, 50, 'Assets/DeathVoid/deathVoid.gif',8)
+        self.deathVoid5 = DeathVoid((WIDTH // 2) + 140, HEIGHT - 50, 450, 50, 'Assets/DeathVoid/deathVoid.gif',10)
+        self.deathVoid6 = DeathVoid((WIDTH // 2) + 208, HEIGHT - 50, 450, 50, 'Assets/DeathVoid/deathVoid.gif',12)
     
     def setScrollSpeed(self, scroll_speed):
         self.scroll_speed = scroll_speed -1
@@ -29,6 +35,13 @@ class Environment():
         self.time = time
 
     def update(self):
+        self.deathVoid0.update_animation()
+        self.deathVoid1.update_animation()
+        self.deathVoid2.update_animation()
+        self.deathVoid3.update_animation()
+        self.deathVoid4.update_animation()
+        self.deathVoid5.update_animation()
+        self.deathVoid6.update_animation()
         wall_delete_list = []
         panel_delete_list = []
         for w in self.walls:
@@ -60,4 +73,10 @@ class Environment():
             w.draw(WIN)
         for p in self.panels:
             p.draw(WIN)
-        self.deathVoid.draw(WIN)
+        self.deathVoid0.draw(WIN)
+        self.deathVoid1.draw(WIN)
+        self.deathVoid2.draw(WIN)
+        self.deathVoid3.draw(WIN)
+        self.deathVoid4.draw(WIN)
+        self.deathVoid5.draw(WIN)
+        self.deathVoid6.draw(WIN)
